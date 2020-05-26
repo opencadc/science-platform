3a70a7daae6f6adcd43312c6a4a7bb188a965783
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2020-05-26 10:38:37 -0700

    CT-140. Updated build files.

--- software-containers/arcade-casa/version-4.5-4.6/startup-patch.sh
+++ software-containers/arcade-casa/version-4.5-4.6/startup-patch.sh
@@ -4,7 +4,7 @@ echo "sourcing admit_start.sh"
 source /opt/admit/admit_start.sh
 
 echo "applying leap second patch"
-casa-config --exec update-data
+/arcade/update-data.patch /opt/casa
 
 echo "starting xterm $1"
 xterm -title $1
