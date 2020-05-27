bcd19a6207c9bb5cbdfc527f5dfc8c1062dd922c
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2020-05-27 09:31:35 -0700

    CT-140 Added more CASA versions and updated build files.

--- software-containers/arcade-casa/version-4.6-latest/download.sh
+++ software-containers/arcade-casa/version-4.6-latest/download.sh
@@ -2,14 +2,20 @@
 
 if [ $# -lt 2 ]
 then
-  echo "usage: $0 <path> <version>"
+  echo "usage: $0 <version> <"el6"|"el7"|"latest">"
   exit 1
 fi
 
-PATH=$1
-VERSION=$2
-FILE="${VERSION}.tar.gz"
-URL="https://casa.nrao.edu/download/distro/${PATH}/${FILE}"
+RELEASE=$1
+FILE="${RELEASE}.tar.gz"
+
+if [ $2 == "el7" ]; then
+  URL="https://casa.nrao.edu/download/distro/casa/release/el7/${FILE}"
+elif [ $2 == "latest" ]; then
+  URL="https://casa.nrao.edu/download/distro/casa-pipeline/release/el7/${FILE}"
+else
+  URL="https://casa.nrao.edu/download/distro/linux/release/el6/${FILE}"
+fi
 
 # make sure we are in the source folder
 HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
