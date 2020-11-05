b7efb371998103cac0796dca5b33e75e9e9b74df
Brian Major <majorb@k9s-dev-login.novalocal>
2020-11-05 00:52:39 +0000

    rename ingress from arcade to skaha

--- arcade-k8s-config/k9s-dev/arcade/certs/create-servops-secret.sh
+++ arcade-k8s-config/k9s-dev/arcade/certs/create-servops-secret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl create -n skaha-system secret generic arcade-servops-secret --from-file=cadcproxy.pem=./clientcert.pem
+kubectl create -n skaha-system secret generic skaha-servops-secret --from-file=cadcproxy.pem=./clientcert.pem
