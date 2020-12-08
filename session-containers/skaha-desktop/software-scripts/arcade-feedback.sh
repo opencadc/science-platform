b29e8078c0cc333b838b295e56500a423c3df0b8
brianmajor <major.brian@gmail.com>
2020-12-08 10:01:31 -0800

    point scripts to correct skaha api

--- session-containers/skaha-desktop/software-scripts/arcade-feedback.sh
+++ session-containers/skaha-desktop/software-scripts/arcade-feedback.sh
@@ -2,4 +2,4 @@
 
 IP_ADDRESS=`hostname --ip-address`
 
-/opt/shibboleth/bin/curl -v -L -k -E /cavern/home/${arcade_username}/.ssl/cadcproxy.pem -d "target-ip=${IP_ADDRESS}" -d "software=canfar-registry.cloud.computecanada.ca/arcade-firefox:latest" --data-urlencode "param=https://github.com/opencadc/arcade/issues" https://${arcade_hostname}/arcade/session/${VNC_PW}/app
+/opt/shibboleth/bin/curl -v -L -k -E /cavern/home/${skaha_username}/.ssl/cadcproxy.pem -d "target-ip=${IP_ADDRESS}" -d "software=canfar-registry.cloud.computecanada.ca/arcade-firefox:latest" --data-urlencode "param=https://github.com/opencadc/arcade/issues" https://${skaha_hostname}/skaha/session/${VNC_PW}/app
