9648cd39e560f2df4efe476b79935792ee5edfd6
Brian Major <major.brian@gmail.com>
2020-12-15 21:10:09 +0000

    deployment touch-ups

--- skaha-k8s-config/keel-dev/skaha/skaha-tomcat/desktop-config/start-desktop-software.sh
+++ skaha-k8s-config/keel-dev/skaha/skaha-tomcat/desktop-config/start-desktop-software.sh
@@ -3,15 +3,15 @@
 USERID=$1
 TITLE=$2
 
-echo "[ARCADE] Starting arcade software container [title=$TITLE] for [userid=$USERID]"
+echo "[skaha] Starting skaha software container [title=$TITLE] for [userid=$USERID]"
 
-INITFILE=/arcade/init.sh
+INITFILE=/skaha/init.sh
 if [ -f "$INITFILE" ]; then
-    echo "[ARCADE] Calling /arcade/init.sh"
-    /arcade/init.sh
+    echo "[skaha] Calling /skaha/init.sh"
+    /skaha/init.sh
 fi
 
-echo "[ARCADE] Starting xterm"
+echo "[skaha] Starting xterm"
 xterm -fg white -bg black -title $TITLE 
 
-echo "[ARCADE] Exit"
+echo "[skaha] Exit"
