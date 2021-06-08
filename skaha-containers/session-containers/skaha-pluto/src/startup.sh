ead921eff895b7343b6c26d3212df513389f5c97
brianmajor <major.brian@gmail.com>
2021-06-08 11:56:33 -0700

    host 0.0.0.0 for docker

--- skaha-containers/session-containers/skaha-pluto/src/startup.sh
+++ skaha-containers/session-containers/skaha-pluto/src/startup.sh
@@ -5,7 +5,7 @@ SESSIONID=$2
 
 echo "Running pluto session $SESSIONID for user $UNAME"
 
-julia -e 'using Pkg; Pkg.add("Pluto"); import Pluto; Pluto.run(require_secret_for_access=false, launch_browser=false, root_url="http://localhost:1234/")'
+julia -e 'using Pkg; Pkg.add("Pluto"); import Pluto; Pluto.run(require_secret_for_access=false, launch_browser=false, host="0.0.0.0")'
 
 echo "Exiting"
 
