6f27d280fc770665100f2c8d12e460c2f7d02b81
brianmajor <major.brian@gmail.com>
2021-12-08 15:40:42 -0800

    Created add-project container

--- deployment/ops-containers/add-user/apply-version.sh
+++ deployment/ops-containers/add-user/apply-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . VERSION && echo "tags: $TAGS"
 for t in $TAGS; do
-   docker image tag images.canfar.net/skaha-system/allocate:latest images.canfar.net/skaha-system/allocate:$t
+   docker image tag images.canfar.net/skaha-system/add-user:latest images.canfar.net/skaha-system/add-user:$t
 done
 unset TAGS
-docker image list images.canfar.net/skaha-system/allocate
+docker image list images.canfar.net/skaha-system/add-user
