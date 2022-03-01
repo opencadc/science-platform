ceb671da77b4ec77fc6a3e03a840fad22f1414ec
Brian Major <majorb@keel-prod-login.novalocal>
2022-03-01 16:45:10 +0000

    use old and new notebook start params

--- deployment/k8s-config/kustomize/base/skaha-workload/config/start-jupyterlab.sh
+++ deployment/k8s-config/kustomize/base/skaha-workload/config/start-jupyterlab.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # One argument expected: the skaha session ID, which makes up part of the connect url
-jupyter lab --ServerApp.ip=0.0.0.0 --ServerApp.port=8888 --no-browser --ServerApp.base_url=notebook/"$1" --ServerApp.root_dir=/ --ServerApp.allow_origin="*" 
+
+jupyter lab --NotebookApp.base_url=notebook/"$1" --NotebookApp.notebook_dir=/ --NotebookApp.allow_origin="*" --ServerApp.ip=0.0.0.0 --ServerApp.port=8888 --no-browser --ServerApp.base_url=notebook/"$1" --ServerApp.root_dir=/ --ServerApp.allow_origin="*" 
