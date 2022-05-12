7f685911058c47ed86f57fa70b63a50bcb2cd2d5
Brian Major <majorb@keel-prod-login.novalocal>
2022-05-12 20:50:57 +0000

    provide /skaha/startup.sh option for desktop software

--- deployment/k8s-config/kustomize/base/skaha-workload/config/start-desktop-software.sh
+++ deployment/k8s-config/kustomize/base/skaha-workload/config/start-desktop-software.sh
@@ -14,12 +14,19 @@ fi
 echo "[skaha] Starting skaha software container [title=$TITLE] for [userid=$USERID]"
 
 INITFILE=/skaha/init.sh
+STARTUPFILE=/skaha/startup.sh
+
 if [ -f "$INITFILE" ]; then
     echo "[skaha] Calling /skaha/init.sh"
     /skaha/init.sh
 fi
 
-echo "[skaha] Starting xterm"
-xterm -fg white -bg black -title $TITLE 
+if [ -f "$STARTUPFILE" ]; then
+    echo "[skaha] Starting xterm via /skaha/startup.sh"
+    /skaha/startup.sh "xterm -fg white -bg black -title $TITLE"
+else
+    echo "[skaha] Starting xterm"
+    xterm -fg white -bg black -title $TITLE
+fi
 
 echo "[skaha] Exit"
