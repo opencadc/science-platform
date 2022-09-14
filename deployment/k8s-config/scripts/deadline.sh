bb9da0702a31a96fbe8bdb3d8af4f986529396e5
Kevin Casteels <kevin.casteels@nrc-cnrc.gc.ca>
2022-09-14 13:19:06 -0700

    Added deadline as a varaible

--- deployment/k8s-config/scripts/deadline.sh
+++ deployment/k8s-config/scripts/deadline.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-JOBFILE=jobs.txt
+
+DEADLINE=1814400
 WORKSPACE=skaha-workload
+JOBFILE=jobs.txt
 
 #Make a list off all jobs, excluding headless batch jobs:
 kubectl -n $WORKSPACE get jobs | grep -Eiv "headless" | awk '{print $1}'ls > $JOBFILE
 
 #Loop through jobs in list and set activeDeadlineSeconds to new value:
 while read J; do
-  kubectl -n $WORKSPACE patch job $J --type='json' -p '[{"op":"add","path":"/spec/activeDeadlineSeconds", "value":1814400}]'
+  kubectl -n $WORKSPACE patch job $J --type='json' -p '[{"op":"add","path":"/spec/activeDeadlineSeconds", "value":'$DEADLINE'}]'
 done <$JOBFILE
 
