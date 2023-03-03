a0efd0c23c4dfb886b297af184e380329a90dd68
Kevin Casteels <kevin.casteels@nrc-cnrc.gc.ca>
2023-03-02 17:17:45 -0800

    Upgraded to v12.3.7

--- deployment/k8s-config/harbor/reapply-harbor-keel-dev.sh
+++ deployment/k8s-config/harbor/reapply-harbor-keel-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-helm upgrade -n cadc-harbor cadc-harbor bitnami/harbor -f values-keel-dev.yaml --version 11.2.0
+helm upgrade -n cadc-harbor cadc-harbor bitnami/harbor -f values-keel-dev.yaml --version 12.3.7
 
