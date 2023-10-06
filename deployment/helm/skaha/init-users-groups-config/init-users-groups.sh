694d399e04a1768ae77f5c01c7d0451db5a41980
Abhishek Ghosh <abhishek.ghosh@thoughtworks.com>
2023-10-06 18:04:30 +0530

    [ Abhishek/Hari | Modifications to run job containers ]

--- deployment/helm/skaha/init-users-groups-config/init-users-groups.sh
+++ deployment/helm/skaha/init-users-groups-config/init-users-groups.sh
@@ -11,14 +11,23 @@ cp /etc/group /etc-group/group
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
-USER_MAPPINGS=$(curl -SsL -k --header "accept: text/plain" --header "authorization: Bearer ${1}" "${2}")
+
+if command -v curl &>/dev/null; then
+  USER_MAPPINGS=$(curl -SsL -k --header "accept: text/plain" --header "authorization: Bearer ${1}" "${2}")
+  GROUP_MAPPINGS=$(curl -SsL -k --header "accept: text/plain" --header "authorization: Bearer ${1}" "${3}")
+elif command -v wget &>/dev/null; then
+  USER_MAPPINGS=$(wget -qO- --header="accept: text/plain" --header="authorization: Bearer ${1}" "${2}")
+  GROUP_MAPPINGS=$(wget -qO- --header="accept: text/plain" --header="authorization: Bearer ${1}" "${3}")
+else
+  echo "Neither curl nor wget is installed. Please install either curl or wget"
+  exit 1
+fi
 
 # USER_MAPPINGS will be in the POSIX format already
 for USER_ENTRY in "${USER_MAPPINGS}"; do
 	echo "${USER_ENTRY}" >> /etc-passwd/passwd
 done
 
-GROUP_MAPPINGS=$(curl -SsL -k --header "accept: text/plain" --header "authorization: Bearer ${1}" "${3}")
 
 # GROUP_MAPPINGS are a list of space delimited URIs to GIDs.  This loop
 # will split at the URI's query ("?") to obtain the group name.
