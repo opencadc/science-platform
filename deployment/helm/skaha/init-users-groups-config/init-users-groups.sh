1af2744a82c4198a6e65b400e2e6bc19ae98c993
Dustin Jenkins <djenkins.cadc@gmail.com>
2024-02-22 14:47:31 -0800

    Restore file to add group and passwd files.

--- deployment/helm/skaha/init-users-groups-config/init-users-groups.sh
+++ deployment/helm/skaha/init-users-groups-config/init-users-groups.sh
@@ -7,32 +7,23 @@
 cp /etc/passwd /etc-passwd/passwd
 cp /etc/group /etc-group/group
 
-# UID_MAP_FILE="/posix-mapping/uidmap.txt"
-# GID_MAP_FILE="/posix-mapping/gidmap.txt"
+UID_MAP_FILE="/posix-mapping/uidmap.txt"
+GID_MAP_FILE="/posix-mapping/gidmap.txt"
 SAVEIFS=$IFS
 IFS='\n'
 
-# if [[ ! -f "${UID_MAP_FILE}" ]]; then
-#     echo "Required file ${UID_MAP_FILE} is missing."
-#     exit 1
-# fi
+if [[ ! -f "${UID_MAP_FILE}" ]]; then
+    echo "Required file ${UID_MAP_FILE} is missing."
+    exit 1
+fi
 
-# if [[ ! -f "${GID_MAP_FILE}" ]]; then
-#     echo "Required file ${GID_MAP_FILE} is missing."
-#     exit 1
-# fi
+if [[ ! -f "${GID_MAP_FILE}" ]]; then
+    echo "Required file ${GID_MAP_FILE} is missing."
+    exit 1
+fi
 
-# cat "${UID_MAP_FILE}" >> /etc-passwd/passwd
-# cat "${GID_MAP_FILE}" >> /etc-group/group
-
-# # restore $IFS
-# IFS=$SAVEIFS
-
-POSIX_UID_MAPPING_URL="${1}"
-POSIX_GID_MAPPING_URL="${2}"
-
-curl -SsL "${POSIX_UID_MAPPING_URL}" >> /etc-passwd/passwd
-curl -SsL "${POSIX_GID_MAPPING_URL}" >> /etc-group/group
+cat "${UID_MAP_FILE}" >> /etc-passwd/passwd
+cat "${GID_MAP_FILE}" >> /etc-group/group
 
 # restore $IFS
 IFS=$SAVEIFS
