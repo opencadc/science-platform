9ab060f5c74ff6cae8238b2b06ee39a82e8968b0
anujachaitanya <anujachaitanya09@gmail.com>
2024-09-19 15:46:13 +0530

    [SP-4265 | Abhishek/AnujaC]Added script for image-caching in redis

---
+++ deployment/helm/skaha/launch-scripts/cache-images.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+harborHosts=($HARBOR_HOST)
+URL="https://$harborHost/api/v2.0/projects?page_size=100"
+PROJECT_URL="https:/$harborHost/api/v2.0/projects"
+
+
+# Fetch the data from the given URL
+redis-cli del temp
+
+for harborHost in "${harborHosts[@]}"; do
+    URL="https://$harborHost/api/v2.0/projects?page_size=100"
+    PROJECT_URL="https://$harborHost/api/v2.0/projects"
+
+    echo "fetching the images host: $URL"
+    response=$(curl -k $URL)
+
+    # Parse the response and iterate over the list
+    echo $response | jq -c '.[]' | while read -r project; do
+        project_name=$(echo $project | jq -r '.name')
+
+        project_data=$(curl -k -s "$PROJECT_URL/$project_name/repositories?page_size=-1")
+
+        # Null check for project_data
+        if [ -z "$project_data" ] || [ "$project_data" == "null" ]; then
+            continue
+        fi
+
+        echo $project_data | jq -c '.[]' | while read -r repo; do
+            repo_name=$(echo $repo | jq -r '.name')
+
+            name=$(echo $repo_name | awk -F'/' '{print $NF}')
+            repo_data=$(curl -k -s "$PROJECT_URL/$project_name/repositories/$name/artifacts?detail=true&with_label=true&page_size=-1")
+
+            echo $repo_data | jq -c '.[]' | while read -r artifact; do
+                tag=$(echo $artifact | jq -r .tags[0].name)
+
+                if [ -z "$tag" ] || [ "$tag" == "null" ]; then
+                    continue
+                fi
+
+                image_id="$harborHost/$name:$tag"
+                labelArray=$(echo $artifact | jq -r .labels)
+
+                # check if labels are empty
+                if [ -z "$labelArray" ] || [ "$labelArray" == "null" ]; then
+                    echo "No labels found for $image_id"
+                    continue
+                fi
+
+                labels=$(echo $artifact | jq -c [.labels[].name])
+
+                refined_artifact=$(echo $artifact | jq -c --argjson labels "$labels" --arg id "$image_id" '{id: $id, types: $labels, digest: .digest, tags: .tags}')
+                echo $refined_artifact | redis-cli -x lpush temp
+            done
+        done
+    done
+done && redis-cli rename temp public
+
+
+
