0f5cdb5aee497a33f2033e92ea7e4522f607695a
Abhishek Ghosh <abhishek.ghosh@thoughtworks.com>
2024-10-15 16:51:13 +0530

    [SP-4263 | Abhishek/Anuja] adding dummy IamGroupUtil

---
+++ skaha/src/main/java/org/opencadc/skaha/utils/IamGroupUtil.java
@@ -0,0 +1,9 @@
+package org.opencadc.skaha.utils;
+
+import java.util.List;
+
+public class IamGroupUtil {
+    public String getParentGroup(List<String> groups) {
+        return "dummy";
+    }
+}
