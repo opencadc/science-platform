adbffc346f51bb5649aff6de0d961ff2b2856fe8
anujachaitanya <anujachaitanya09@gmail.com>
2024-10-21 15:01:01 +0530

    [SP-4263 | Abhishek/Anuja] Added test for QueueUtil

--- skaha/src/test/java/org/opencadc/skaha/utils/QueueUtilTest.java
+++ skaha/src/test/java/org/opencadc/skaha/utils/QueueUtilTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 
@@ -24,7 +25,6 @@ public class QueueUtilTest {
                     "skaha-workload-queue-interactive::carta,notebook,desktop,contributed,desktop-app"
             };
 
-            // Set up expectations
             mockedStatic.when(() -> CommandExecutioner.execute(any(String[].class), eq(false)))
                     .thenReturn(String.join("\n", localQueues));
 
@@ -33,6 +33,40 @@ public class QueueUtilTest {
             assertEquals("skaha-workload-queue-headless", actualLocalQueue);
         }
 
+    }
 
+    @Test
+    public void testShouldThrowExceptionIfNoLocalqueueIsAvailable() {
+        try (MockedStatic<CommandExecutioner> mockedStatic = Mockito.mockStatic(CommandExecutioner.class)) {
+
+            List<String> groupNames = List.of("skaha-workload-queue-headless");
+            String jobType = "headless";
+
+            mockedStatic.when(() -> CommandExecutioner.execute(any(String[].class), eq(false)))
+                    .thenReturn("");
+
+            RuntimeException e = assertThrows(RuntimeException.class, ()-> QueueUtil.getLocalQueue(groupNames, jobType));
+            assertEquals(e.getMessage(), "No LocalQueue available");
+        }
+    }
+
+    @Test
+    public void testShouldThrowExceptionForMultipleLocalQueuesForGroup() {
+        try (MockedStatic<CommandExecutioner> mockedStatic = Mockito.mockStatic(CommandExecutioner.class)) {
+
+            List<String> groupNames = List.of("skaha-workload-queue-headless");
+            String jobType = "headless";
+            String[] localQueues = {
+                    "skaha-workload-queue-headless::headless",
+                    "skaha-workload-queue-interactive::carta,notebook,desktop,contributed,desktop-app",
+                    "skaha-workload-queue-headless-another::headless"
+            };
+
+            mockedStatic.when(() -> CommandExecutioner.execute(any(String[].class), eq(false)))
+                    .thenReturn(String.join("\n", localQueues));
+
+            RuntimeException e = assertThrows(RuntimeException.class, ()-> QueueUtil.getLocalQueue(groupNames, jobType));
+            assertEquals(e.getMessage(), "More than one LocalQueue for a group is Unsupported");
+        }
     }
 }
