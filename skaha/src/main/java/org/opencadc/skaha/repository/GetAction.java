128c57a211fc09293e885f971631df5cf5e397af
Dustin Jenkins <djenkins.cadc@gmail.com>
2024-11-14 18:31:18 +0000

    feat: expose repository hosts through authenticated api call

---
+++ skaha/src/main/java/org/opencadc/skaha/repository/GetAction.java
@@ -0,0 +1,43 @@
+package org.opencadc.skaha.repository;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import org.json.JSONWriter;
+import org.opencadc.skaha.SkahaAction;
+
+/**
+ * Output the resource context information.
+ *
+ * @author majorb
+ */
+public class GetAction extends SkahaAction {
+
+    @Override
+    public void doAction() throws Exception {
+        initRequest();
+        final Writer writer = initWriter();
+        final JSONWriter jsonWriter = new JSONWriter(writer).array();
+        try {
+            Arrays.stream(getHarborHosts()).forEach(jsonWriter::value);
+        } finally {
+            jsonWriter.endArray();
+            writer.flush();
+        }
+    }
+
+    @Override
+    protected void initRequest() throws Exception {
+        super.initRequest();
+    }
+
+    Writer initWriter() throws IOException {
+        this.syncOutput.setHeader("content-type", "application/json");
+        return new OutputStreamWriter(syncOutput.getOutputStream());
+    }
+
+    String[] getHarborHosts() {
+        return this.harborHosts.toArray(new String[0]);
+    }
+}
