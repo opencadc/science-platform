2b6676c7e4cf5aa5b77189103be6aa8045167c79
Dustin Jenkins <djenkins.cadc@gmail.com>
2025-01-16 16:18:46 -0800

    fix: correct script when removing middlewares and add debug

--- deployment/k8s-config/scripts/cleanup.sh
+++ deployment/k8s-config/scripts/cleanup.sh
@@ -4,25 +4,42 @@
 # The script can be executed in its current directory by typing:
 # ./cleanup.sh
 
-echo "START"
+# To DEBUG first:
+# CLEANUP_DEBUG=1 ./cleanup.sh
+
+echo "START $(date -u +"%Y%m%dT%H%M%S")"
+
+if [[ "${CLEANUP_DEBUG}" == "1" ]]
+then
+  echo "DEBUG MODE"
+fi
 
 types="service ingressroute middleware"
 for type in $types;
 do
-  kubectl -n skaha-workload get $type |
+  kubectl -n skaha-workload get --no-headers=true $type |
     while IFS= read -r line
     # for each service session
     do
       # obtain various info on the session
       name=`echo $line | awk '{print $1}'`
-      id=`echo $name | awk -F- '{print $4}'`
+      id=`echo $name | awk -F- '{print $NF}'`
 
       # check if there is a corresponding pod
-      out=`kubectl -n skaha-workload get pods | grep -e "$id"`
+      out=`kubectl -n skaha-workload get pods --ignore-not-found=true -l canfar-net-sessionID="${id}"`
       if [[ -z "$out" && -n "$id" ]]
       then
+        CMD="kubectl -n skaha-workload delete $type $name"
+        echo "${CMD}"
+
         # no corresponding pod, delete orphaned session
-        kubectl -n skaha-workload delete $type $name
+	      if [[ -z "${CLEANUP_DEBUG}" ]]
+	      then
+	        echo "Exec: ${CMD}"
+	      fi
+      elif [[ "${CLEANUP_DEBUG}" == "1" ]]
+      then
+	      echo "Nothing to do for Session ${id}"
       fi
     done
 done
