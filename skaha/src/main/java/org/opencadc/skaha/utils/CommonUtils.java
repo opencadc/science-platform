325ceaf7240779fd95b864bbcbafeae0697e2b5a
Dustin Jenkins <djenkins.cadc@gmail.com>
2025-04-17 13:21:19 -0700

    refactor: remove unused code

--- skaha/src/main/java/org/opencadc/skaha/utils/CommonUtils.java
+++ skaha/src/main/java/org/opencadc/skaha/utils/CommonUtils.java
@@ -1,7 +1,6 @@
 package org.opencadc.skaha.utils;
 
 import ca.nrc.cadc.reg.client.LocalAuthority;
-import java.math.BigDecimal;
 import java.net.URI;
 import java.util.Base64;
 import java.util.Collection;
@@ -30,41 +29,5 @@ public class CommonUtils {
     public static URI firstLocalServiceURI(final URI baseStandardID) {
         final Set<URI> serviceURIs = new LocalAuthority().getServiceURIs(baseStandardID);
         return serviceURIs.stream().findFirst().orElseThrow(IllegalStateException::new);
-    }
-
-    /**
-     * Format to Binary SI values, which users powers of 1000, not 1024.
-     *
-     * @param bytes Bytes reported from Kubernetes.
-     * @return String formatted, never null.
-     */
-    public static String formatMemoryFromBytes(final BigDecimal bytes) {
-        final long longBytes = bytes.longValue();
-        final long absB = longBytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(longBytes);
-
-        if (absB < 1024) {
-            return longBytes + " B";
-        }
-
-        final String[] units = {"Ki", "Mi", "Gi", "Ti", "Pi", "Ei"};
-        int unitIndex = -1;
-        double value = longBytes;
-
-        do {
-            value = value / 1024.0D;
-            unitIndex++;
-        } while (value >= 1024.0D && unitIndex < units.length - 1);
-
-        return String.format("%d%s", Double.valueOf(value).longValue(), units[unitIndex]);
-    }
-
-    /**
-     * Format the given number of cores, likely in decimal form, as a String integer.
-     *
-     * @param cores Cores in BigDecimal format.
-     * @return String formatted, never null.
-     */
-    public static String formatCPUCores(final BigDecimal cores) {
-        return Integer.toString(cores.intValue());
     }
 }
