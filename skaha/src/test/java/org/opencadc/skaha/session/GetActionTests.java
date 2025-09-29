66636c2e49e8e6cde311bbde3b5c76f4ce0241f3
Dustin Jenkins <djenkins.cadc@gmail.com>
2025-09-29 11:16:45 -0700

    ci: fixing test linting

--- skaha/src/test/java/org/opencadc/skaha/session/GetActionTests.java
+++ skaha/src/test/java/org/opencadc/skaha/session/GetActionTests.java
@@ -210,22 +210,12 @@ public class GetActionTests {
 
     @Before
     public void setupHomePath() {
-        System.setProperty("SKAHA_USER_STORAGE_TOP_LEVEL_DIRECTORY", "/tmp");
-        System.setProperty("SKAHA_USER_STORAGE_HOME_BASE_DIRECTORY", "/tmp/skaha-test");
-        System.setProperty("SKAHA_USER_STORAGE_PROJECTS_BASE_DIRECTORY", "/tmp/skaha-test-projects");
-        System.setProperty("SKAHA_USER_STORAGE_SERVICE_URI", "ivo://example.org/skaha/userStorage");
-        System.setProperty("SKAHA_USER_STORAGE_USER_HOME_URI", "vos://example.org~skaha/home");
-        System.setProperty("SKAHA_USER_STORAGE_USER_PROJECTS_URI", "vos://example.org~skaha/projects");
+        TestUtil.setupUserStorageEnvironment();
     }
 
     @After
     public void removeHomePath() {
-        System.getProperties().remove("SKAHA_USER_STORAGE_TOP_LEVEL_DIRECTORY ");
-        System.getProperties().remove("SKAHA_USER_STORAGE_HOME_BASE_DIRECTORY");
-        System.getProperties().remove("SKAHA_USER_STORAGE_PROJECTS_BASE_DIRECTORY");
-        System.getProperties().remove("SKAHA_USER_STORAGE_SERVICE_URI");
-        System.getProperties().remove("SKAHA_USER_STORAGE_USER_HOME_URI");
-        System.getProperties().remove("SKAHA_USER_STORAGE_USER_PROJECTS_URI");
+        TestUtil.tearDownUserStorageEnvironment();
     }
 
     @Test
